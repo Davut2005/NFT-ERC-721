af2fdd82d7b6532a2f83039b60c63a2c88e840d5
Davut <2005davut55@gmail.com>
2024-12-01 22:50:30 +0200

    Creating NFT and implementing the payment transaction for creating NFT in smart contracts

--- Web3Builders.sol
+++ Web3Builders.sol
@@ -2,17 +2,23 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 
-contract Web3Token is ERC721, ERC721Enumerable, ERC721Pausable {
+contract DavutToken is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
     uint256 private _nextTokenId;
     bool public publicMintOpen = false;
     bool public allowListMintOpen = false;
-    uint256 public maxSupply = 1;
+    uint256 public maxSupply = 2000;
 
-    constructor() payable ERC721("Web3Token", "Web3") {}
+    mapping ( address => bool ) public allowList;
+
+    constructor(address initialOwner) 
+      ERC721("Web3Token", "Web3")
+      Ownable(initialOwner) 
+    {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://QmY5rPqGTN1rZxMQg2ApiSZc7JiBNs1ryDzXPZpQhC1ibm/";
@@ -35,19 +41,28 @@ contract Web3Token is ERC721, ERC721Enumerable, ERC721Pausable {
     }
 
     function allowListMint() public payable {
-        require( publicMintOpen, "AllowListMint is not available" );
-        require( totalSupply() < maxSupply, "We sold out!" );
+        require( allowListMintOpen, "AllowListMint is not available" );
+        require ( allowList[msg.sender], "You are not in the allowedList" );
         require( msg.value == 0.001 ether, "Not enough funds!" );
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(msg.sender, tokenId);
+        internalMint();
     }
     
     function publicMint() public payable {
         require( publicMintOpen, "PublicMint is not available" );
+        require( msg.value == 0.01 ether, "Not enough funds! You need money!" );
+        internalMint();
+    }
+
+    function internalMint () internal {
         require( totalSupply() < maxSupply, "We sold out!" );
-        require( msg.value == 0.01 ether, "Not enough funds!" );
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
+    }
+
+    function setAllowList ( address[] calldata addresses ) external onlyOwner {
+        for ( uint i = 0; i < addresses.length; i++ ) {
+            allowList[addresses[i]] = true;
+        }
     }
     // The following functions are overrides required by Solidity.
 
